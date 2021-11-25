891234ec9d588f9d8a8fd780447f344ddcb8a5c0
rudysemola <r.semola@studenti.unipi.it>
2021-11-25 18:03:34 +0100

    PR DONE minor change in experiment.py

--- strategies/iCARL/experiment.py
+++ strategies/iCARL/experiment.py
@@ -1,44 +1,21 @@
 import unittest
 
-from os.path import expanduser
-
+import numpy as np
 import torch
-
-from avalanche.benchmarks.datasets import CIFAR100
-from avalanche.benchmarks.utils import AvalancheDataset
-from avalanche.models import IcarlNet, make_icarl_net, initialize_icarl_net
-from avalanche.training.plugins.lr_scheduling import LRSchedulerPlugin
+from torch.optim.lr_scheduler import MultiStepLR
 from torch.optim import SGD
 from torchvision import transforms
-from avalanche.benchmarks.generators import nc_benchmark
+
+from strategies.utils import get_target_result, get_average_metric, create_default_args
+
+from avalanche.benchmarks.classic.ccifar100 import SplitCIFAR100
+from avalanche.models import IcarlNet, make_icarl_net, initialize_icarl_net
+from avalanche.training.plugins.lr_scheduling import LRSchedulerPlugin
 from avalanche.training.plugins import EvaluationPlugin
 from avalanche.evaluation.metrics import *
 from avalanche.logging.interactive_logging import InteractiveLogger
-import random
-import numpy as np
-from torch.optim.lr_scheduler import MultiStepLR
 
-from strategies.utils import get_average_metric, create_default_args
 from avalanche.training.strategies.icarl import ICaRL
-
-
-def get_dataset_per_pixel_mean(dataset):
-    result = None
-    patterns_count = 0
-
-    for img_pattern, _ in dataset:
-        if result is None:
-            result = torch.zeros_like(img_pattern, dtype=torch.float)
-
-        result += img_pattern
-        patterns_count += 1
-
-    if result is None:
-        result = torch.empty(0, dtype=torch.float)
-    else:
-        result = result / patterns_count
-
-    return result
 
 
 def icarl_cifar100_augment_data(img):
@@ -94,41 +71,15 @@ class iCARL(unittest.TestCase):
                               if torch.cuda.is_available() and
                                  args.cuda >= 0 else "cpu")
 
-        per_pixel_mean = get_dataset_per_pixel_mean(
-            CIFAR100(expanduser("~") + "/.avalanche/data/cifar100/",
-                     train=True, download=True,
-                     transform=transforms.Compose([transforms.ToTensor()])))
+        benchmark = SplitCIFAR100(n_experiences=args.nb_exp, seed=args.seed,
+                      fixed_class_order=args.fixed_class_order)
 
-        transforms_group = dict(
-            eval=(transforms.Compose([
-                transforms.ToTensor(),
-                lambda img_pattern: img_pattern - per_pixel_mean]), None),
-            train=(transforms.Compose([
-                transforms.ToTensor(),
-                lambda img_pattern: img_pattern - per_pixel_mean,
-                icarl_cifar100_augment_data]), None),
-        )
+        interactive_logger = InteractiveLogger()
+        eval_plugin = EvaluationPlugin(
+            accuracy_metrics(experience=True, stream=True),
+            loggers=[interactive_logger])
 
-        train_set = CIFAR100(expanduser("~") + "/.avalanche/data/cifar100/",
-                             train=True, download=True, )
-        test_set = CIFAR100(expanduser("~") + "/.avalanche/data/cifar100/",
-                            train=False, download=True, )
-
-        train_set = AvalancheDataset(train_set,
-                                     transform_groups=transforms_group,
-                                     initial_transform_group='train')
-        test_set = AvalancheDataset(test_set,
-                                    transform_groups=transforms_group,
-                                    initial_transform_group='eval')
-
-        scenario = nc_benchmark(
-            train_dataset=train_set,
-            test_dataset=test_set,
-            n_experiences=args.nb_exp,
-            task_labels=False, seed=args.seed,
-            shuffle=False,
-            fixed_class_order=args.fixed_class_order)
-
+        # _____________________________Strategy
         model: IcarlNet = make_icarl_net(num_classes=100)
         model.apply(initialize_icarl_net)
 
@@ -136,11 +87,6 @@ class iCARL(unittest.TestCase):
                     weight_decay=args.wght_decay, momentum=0.9)
         sched = LRSchedulerPlugin(
             MultiStepLR(optim, args.lr_milestones, gamma=1.0 / args.lr_factor))
-
-        interactive_logger = InteractiveLogger()
-        eval_plugin = EvaluationPlugin(
-            accuracy_metrics(experience=True, stream=True),
-            loggers=[interactive_logger])
 
         strategy = ICaRL(
             model.feature_extractor, model.classifier, optim,
@@ -153,15 +99,14 @@ class iCARL(unittest.TestCase):
 
         # Dict to iCaRL Evaluation Protocol: Average Incremental Accuracy
         dict_iCaRL_aia = {}
-        # Training and eval
-        for i, exp in enumerate(scenario.train_stream):
-            eval_exps = [e for e in scenario.test_stream][:i + 1]
+        # ___________________________________________train and eval
+        for i, exp in enumerate(benchmark.train_stream):
             strategy.train(exp, num_workers=4)
-            res = strategy.eval(eval_exps, num_workers=4)
+            res = strategy.eval(benchmark.test_stream[:i + 1], num_workers=4)
             dict_iCaRL_aia['Top1_Acc_Stream/Exp'+str(i)] = res['Top1_Acc_Stream/eval_phase/test_stream/Task000']
 
         avg_ia = get_average_metric(dict_iCaRL_aia)
-        target_acc = 0.641  # get_target_result('iCaRL', 'benchmark')
+        target_acc = get_target_result('iCaRL', 'scifar100')
         print("dict_iCaRL_aia= ", dict_iCaRL_aia)
         print(f"scifar100-batch=10 Average Incremental Accuracy: {avg_ia:.5f}")
 
