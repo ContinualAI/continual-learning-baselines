954836c2b7cd9b3e57cc333c3b5310e970c41813
AndreaCossu <andrea.cossu@sns.it>
2022-05-26 16:38:17 +0200

    Skipped template test

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,24 +1,19 @@
 import unittest
 import torch
 import avalanche as avl
-from tests.utils import create_default_args, get_average_metric, get_target_result
+from utils import get_average_metric, get_target_result
 
 
+@unittest.skip("Just a template, skipping this test.")  # remove this when implementing the test
 class StrategyName(unittest.TestCase):
-    def test_benchmarkname(self, override_args=None):
-        #####################
-        # Create arguments  #
-        #####################
-        # add as many parameters as you want in the input dictionary
-        args = create_default_args({'cuda': 0}, override_args)
-        device = torch.device(f"cuda:{args.cuda}"
-                              if torch.cuda.is_available() and
-                              args.cuda >= 0 else "cpu")
+    def test_benchmarkname(self):
+        pass
 
         #####################
         # FILL HERE         #
         #####################
         # Create your experiment with Avalanche and put it in the `experiments` folder
+        # in the project root directory
         # get the final results into the res variable
         # res = mystrategy_benchmark(args)
 
@@ -27,7 +22,13 @@ class StrategyName(unittest.TestCase):
         #####################
         # you may find useful the already imported functions
         # `get_average_metric` and `get_target_result`
-        if args.check:
-            pass
-            # check that your current result meets the expected result
-            # e.g., self.assertAlmostEqual(target_acc, expected_acc, delta=0.02)
+
+        # example:
+        # acc = get_average_metric(res)
+        # target_acc = float(get_target_result('strategy', 'benchmark'))
+
+        # check that your current result meets the expected result
+
+        # example:
+        # if target_acc > avg_stream_acc:
+        #   self.assertAlmostEqual(target_acc, avg_stream_acc, delta=0.02)
