de262fdcdb3e710d04b481c16d479cbef39173b1
AndreaCossu <andrea.cossu@sns.it>
2023-03-16 11:56:32 +0100

    Added import of cifar10 in experiments

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -2,5 +2,6 @@ from . import split_mnist
 from . import permuted_mnist
 from . import split_tiny_imagenet
 from . import split_cifar100
+from . import split_cifar10
 from . import core50
 from . import utils
