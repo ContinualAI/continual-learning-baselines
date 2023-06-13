c5d60680115b2089da85c15768b49849a449b143
AndreaCossu <andrea.cossu@di.unipi.it>
2023-06-13 19:01:39 +0200

    Reduced test duration for gem-pmnist

--- experiments/permuted_mnist/gem.py
+++ experiments/permuted_mnist/gem.py
@@ -1,25 +1,10 @@
 import avalanche as avl
 import torch
-from avalanche.benchmarks.utils import AvalancheSubset
-from avalanche.benchmarks.utils.data_loader import TaskBalancedDataLoader
 from torch.nn import CrossEntropyLoss
 from torch.optim import SGD
 from avalanche.evaluation import metrics as metrics
 from models import MLP
 from experiments.utils import set_seed, create_default_args
-
-
-class GEM_reduced(avl.training.GEM):
-    def make_train_dataloader(self, num_workers=0, shuffle=True,
-                              pin_memory=True, **kwargs):
-        """Select only 1000 patterns for each experience as in GEM paper."""
-        self.dataloader = TaskBalancedDataLoader(
-            AvalancheSubset(self.adapted_dataset, indices=list(range(1000))),
-            oversample_small_groups=True,
-            num_workers=num_workers,
-            batch_size=self.train_mb_size,
-            shuffle=shuffle,
-            pin_memory=pin_memory)
 
 
 def gem_pmnist(override_args=None):
@@ -29,14 +14,14 @@ def gem_pmnist(override_args=None):
     """
     args = create_default_args({'cuda': 0, 'patterns_per_exp': 1000, 'hidden_size': 100,
                             'hidden_layers': 2, 'epochs': 1, 'dropout': 0,
-                            'mem_strength': 0.5,
+                            'mem_strength': 0.5, 'n_exp': 17,
                             'learning_rate': 0.1, 'train_mb_size': 10, 'seed': None}, override_args)
     set_seed(args.seed)
     device = torch.device(f"cuda:{args.cuda}"
                           if torch.cuda.is_available() and
                           args.cuda >= 0 else "cpu")
 
-    benchmark = avl.benchmarks.PermutedMNIST(20)
+    benchmark = avl.benchmarks.PermutedMNIST(args.n_exp)
     model = MLP(hidden_size=args.hidden_size, hidden_layers=args.hidden_layers,
                 drop_rate=args.dropout)
     criterion = CrossEntropyLoss()
@@ -47,7 +32,7 @@ def gem_pmnist(override_args=None):
         metrics.accuracy_metrics(epoch=True, experience=True, stream=True),
         loggers=[interactive_logger])
 
-    cl_strategy = GEM_reduced(
+    cl_strategy = avl.training.GEM(
         model, SGD(model.parameters(), lr=args.learning_rate), criterion,
         patterns_per_exp=args.patterns_per_exp, memory_strength=args.mem_strength,
         train_mb_size=args.train_mb_size, train_epochs=args.epochs, eval_mb_size=128,
